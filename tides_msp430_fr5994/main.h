2ed6a1fb0c01c09479078310b361aea0aba1d4e8
liusongran <me.songran@gmail.com>
2023-12-12 22:16:06 +0800

    test performance

--- tides_msp430_fr5994/main.h
+++ tides_msp430_fr5994/main.h
@@ -21,7 +21,9 @@ static void __cs_init(){
      * Configure one FRAM waitstate as required by the device datasheet for MCLK
      * operation beyond 8MHz _before_ configuring the clock system.
      */
-    FRCTL0 = FRCTLPW | AUTO_1;
+    //FRCTL0 = FRCTLPW | AUTO_1;
+    FRCTL0 = FRCTLPW | AUTO_0 | NWAITS_3;
+    //FRCTL0 = FRCTLPW | AUTO_0;
 
     CS_initClockSignal(CS_MCLK,CS_DCOCLK_SELECT,CS_CLOCK_DIVIDER_1);
     CS_initClockSignal(CS_SMCLK,CS_DCOCLK_SELECT,CS_CLOCK_DIVIDER_1);
